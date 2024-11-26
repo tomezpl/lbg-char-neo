5aa70e0ba3e31e2ad93158dbe60aab9fd7f8e5b1
Tomasz Zajac <tomezpl@gmail.com>
2024-11-26 20:04:52 +0000

    chore(nativeui): removed unused function

--- client/ui/native-ui-wrapper.ts
+++ client/ui/native-ui-wrapper.ts
@@ -20,7 +20,6 @@ interface MenuGlobal {
     Clear(menu: Menu): void;
     AddInstructionButton(menu: Menu, button: string, text: string): InstructionButton;
     CurrentSelection(menu: Menu): number;
-    DisEnableControls(menu: Menu, controlsEnabled: boolean): void;
 }
 
 interface MenuItemGlobal {
@@ -139,8 +138,7 @@ const exportsKeys: Record<keyof Omit<INativeUIRoot, 'setEventListener'> | AllNes
     'MenuListItem:doesPanelExist': 1,
     'Menu:Clear': 1,
     'Menu:AddInstructionButton': 1,
-    'Menu:CurrentSelection': 1,
-    'Menu:DisEnableControls': 1
+    'Menu:CurrentSelection': 1
 };
 
 type EventTarget = Menu | MenuItem;
